261bf377054f6e3543c4ace56f3cb16bd1c18603
nhern121 <nhern121@fiu.edu>
2018-10-01 15:31:56 -0400

    binary search

--- cracking/DP/binarysearch.py
+++ cracking/DP/binarysearch.py
@@ -1,27 +1,57 @@
-def binary_search(vector, val):
-        low = 0
-        upp = len(vector)-1
+#not working 
+#def binary_search(vector, val):
+#        low = 0
+#        upp = len(vector)-1
 
-        while 1:
-                range = upp - low
-                if range==0 and vector[low]!=val:
-                        print('Element not in array')
-                        return
-                if vector[low]>vector[upp]:
-                        print('vector is not sorted')
-                        return
-                cent = ((range)/2) + low
-                if val == vector[cent]:
-                        return cent
-                elif val < vector[cent]:
-                        upp = cent - 1
-                else:
-                        low = cent + 1
+	#if vector[upp] < target:
+	#	print('ELement not in array')
+	#	return
+
+#        while 1:
+#                range = upp - low
+#                if range==0 and vector[low]!=val:
+#                        print('Element not in array')
+#                        return
+#                if vector[low]>vector[upp]:
+#                        print('vector is not sorted')
+#                        return
+#                cent = ((range)//2) + low
+#                if val == vector[cent]:
+#                        return cent
+#                elif val < vector[cent]:
+#                        upp = cent - 1
+#                else:
+#                        low = cent + 1
+
+##############################################################################
+def binarySearch(arr, x): 
+	l =0
+	r = len(arr)-1
+
+	while l <= r: 
+
+        	mid = l + (r - l)//2; 
+          
+        	# Check if x is present at mid 
+        	if arr[mid] == x: 
+            		return mid 
+  
+        	# If x is greater, ignore left half 
+        	elif arr[mid] < x: 
+            		l = mid + 1
+  
+        	# If x is smaller, ignore right half 
+        	else: 
+            		r = mid - 1
+      
+   	 # If we reach here, then the element 
+    	# was not present 
+	return False
 
 
-print(binary_search([2,4,5,7,8,10],10))
+#print(binary_search([2,4,5,7,8,10],7))
 
-
+#print(binary_search([10,11,16,20],12))
 
 
 
