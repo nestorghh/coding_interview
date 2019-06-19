5b1b3e0125ac37bcfe38fff5168202ac88b57cab
nhern121 <nhern121@fiu.edu>
2019-06-18 23:30:34 -0400

    remove element leetcode #27

---
+++ remove_element.py
@@ -0,0 +1,14 @@
+def removeElement(nums,val):
+	count=0
+	for i in range(len(nums)):
+		if nums[i]!=val:
+			nums[count]=nums[i]
+			count+=1
+	return count, nums
+
+print(removeElement([3,2,2,3],3))
+
+print(removeElement([0,1,2,3,3,0,4,2],2))
+
+print(removeElement([0,0,0,0],0))
+
