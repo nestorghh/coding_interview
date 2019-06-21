23c3e4f52a60963aec5941dcd79bd1fc98cb87c5
nhern121 <nhern121@fiu.edu>
2019-06-21 19:03:26 -0400

    Best time to buy and sell stock

---
+++ maximum_profit.py
@@ -0,0 +1,35 @@
+#Leetcode #121. Best Time to Buy and Sell Stock
+
+def maxprofit_bf(prices):
+	maxi=0
+	for i in range(len(prices)-1):
+		for j in range(i+1,len(prices)):
+			margin = prices[j]-prices[i]
+			if margin>maxi:
+				maxi=margin
+
+	return maxi
+
+
+print(maxprofit_bf([7,1,5,3,6,4]))
+print(maxprofit_bf([7,6,4,3,1]))
+
+
+###############################################################
+
+def maxprofit_linear(prices):
+	mini=10**6
+	maxi=0
+	for i in range(len(prices)):
+		if prices[i]<mini:
+			mini = prices[i]
+		if (prices[i]-mini)>maxi:
+			maxi = prices[i] - mini
+	return maxi
+	
+	
+print(maxprofit_linear([7,1,5,3,6,4]))
+print(maxprofit_linear([7,6,4,3,1]))
+
+#Runtime: 40 ms, faster than 87.92% of Python3 online submissions for Best Time to Buy and Sell Stock.#Memory Usage: 13.7 MB, less than 97.41% of Python3 online submissions for Best Time to Buy and Sell Stock.
+
