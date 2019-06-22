dc4459322f2137aa303fab914c527c12696f1a64
nhern121 <nhern121@fiu.edu>
2019-06-21 21:28:30 -0400

    Majority element

--- majority_element.py
+++ majority_element.py
@@ -10,8 +10,21 @@ def majority(numbers):
 
 print(majority([3,2,3]))
 print(majority([2,2,1,1,1,2,2]))
+print(majority([3,1,2,1,6,7,3,3,3]))
 
 #Runtime: 52 ms, faster than 64.65% of Python3 online submissions for Majority Element.
 #Memory Usage: 14.4 MB, less than 51.79% of Python3 online submissions for Majority Element.
 
+#############################################################
+
+def majority_v2(numbers):
+	numbers.sort()
+	return numbers[len(numbers)//2]
+
+print(majority_v2([3,2,3])) 
+print(majority_v2([2,2,1,1,1,2,2])) 
+print(majority_v2([3,1,2,1,6,7,3,3,3])) 
+
+#Runtime: 52 ms, faster than 64.65% of Python3 online submissions for Majority Element.
+#Memory Usage: 14.2 MB, less than 91.39% of Python3 online submissions for Majority Element.
 
