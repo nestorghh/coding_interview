0b8fdb49ccf89a113cc86634ac084728c2ea2934
nhern121 <nhern121@fiu.edu>
2019-06-25 12:27:09 -0400

    house robber via dp

--- house_robber.py
+++ house_robber.py
@@ -1,6 +1,8 @@
 
 # rethink this. this is incorrect.
 def house_robber(nums):
+	if len(nms)==0:
+		return 0
 	temp=[0]*(len(nums)+1)
 	temp[1]=nums[0]
 	for i in range(2,len(temp)):
@@ -10,4 +12,8 @@ def house_robber(nums):
 print(house_robber([1,2,3,1]))
 print(house_robber([2,7,9,3,1]))
 print(house_robber([2,1,1,2]))
+
+
+#Runtime: 32 ms, faster than 91.74% of Python3 online submissions for House Robber.
+#Memory Usage: 13.2 MB, less than 50.40% of Python3 online submissions for House Robber.
 
