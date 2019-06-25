23a183979ad7146620ab115de0f08d3c8217ad96
nhern121 <nhern121@fiu.edu>
2019-06-25 12:04:53 -0400

    house robber via dp

--- house_robber.py
+++ house_robber.py
@@ -1,13 +1,13 @@
 
 # rethink this. this is incorrect.
 def house_robber(nums):
-	sum2=0
-	for i in range(0,len(nums),2):
-		sum2+= nums[i]
-		
-	return max(sum2, sum(nums)-sum2)
+	temp=[0]*(len(nums)+1)
+	temp[1]=nums[0]
+	for i in range(2,len(temp)):
+		temp[i] = max(temp[i-1],temp[i-2]+nums[i-1])
+	return temp[-1]
 
 print(house_robber([1,2,3,1]))
 print(house_robber([2,7,9,3,1]))
-1000 --->9.9
-211000--->x
+print(house_robber([2,1,1,2]))
+
