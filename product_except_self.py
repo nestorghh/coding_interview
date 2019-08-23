ea1df0a9d6861bdd7e480fbb19727cfbd536a649
nestorghh <nestorghh@outlook.com>
2019-08-22 22:02:33 -0400

    except

--- product_except_self.py
+++ product_except_self.py
@@ -18,4 +18,34 @@ def productExceptSelf(nums):
 
 
 
+def productExcept(nums):
+
+	def multiply(a):
+		p=1
+		for e in a:
+			p*=e
+		return p
+
+	return [multiply(nums[:i])* multiply(nums[i+1:]) for i in range(len(nums))]
+
+
+def productExcept2(nums):
+	l=len(nums)
+	answer = [0]*l
+
+	L, R, answer = [0]*l, [0]*l, [0]*l
+
+	L[0]=1
+	for i in range(1,l):
+		L[i] = nums[i-1] * L[i-1]
+
+	R[l-1]=1
+	for i in reversed(range(l-1)):
+		R[i] = nums[i+1] * R[i+1]
+
+	for i in range(l):
+		answer[i] = L[i] * R[i]
+
+	return answer
+
 
