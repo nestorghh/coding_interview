ad59ebcf40a918bd1444d955f227e555cbd6167c
nhern121 <nhern121@fiu.edu>
2019-09-09 21:33:15 -0400

    merge intervals revisited

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -15,6 +15,23 @@ def mergeIntervals(I):
 		print(merged) 
 	return merged
 	
+def merge(intervals):
+	merged=[]
+	intervals.sort(key=lambda x:x[0])
+	
+	merged.append(intervals[0])
+
+	for i in range(1,len(intervals)):
+		top=merged[-1]
+		
+		if top[1]<intervals[i][0]:
+			merged.append(intervals[i])
+		elif top[1]<intervals[i][1]:
+			top[1]=intervals[i][1]
+			merged[-1]=top
+	return merged
 
 
 
+intervals=[[1,3],[2,6],[8,10],[15,18]]
+	
