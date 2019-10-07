8bdd0869d84cf42de5940dee626de8645ea3aba6
nhern121 <nhern121@fiu.edu>
2019-10-07 00:24:11 -0400

    daily temperatures with a stack

--- dailyTemperatures.py
+++ dailyTemperatures.py
@@ -1,21 +1,15 @@
 def dailyTemperatures(T):
 	n=len(T)
-	r=[]
+	ans=[0]*n
+	stack=[]
 
-	for i in range(n-1):
-		c=0
-		for j in range(i+1,n):
-			if T[j]>T[i]:
-				c=1
-				break
-			if T[j]<T[i]:
-				c+=1
+	for i in range(n-1,-1,-1):
+		while stack and T[i]>=T[stack[-1]]:
+			stack.pop()
+		if stack:
+			ans[i] = stack[-1] - i
 
-		if c==(n-i-1):
-			c=0
-		r.append(c)
-	return r
+		stack.append(i)
 
-
-
+	return ans
 
