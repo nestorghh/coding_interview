a8894e17c5917398be1f2a76388f418d9bb26fbe
nestorghh <nestorghh@outlook.com>
2019-11-26 12:19:20 -0500

    two sum fixed

--- twoSum.py
+++ twoSum.py
@@ -1,13 +1,14 @@
 def twoSum(nums,target):
 	d={}
 	for i in range(0,len(nums)):
+		print(target -nums[i])
 		if target - nums[i] not in d:
 			d[nums[i]]=i
-	else:
+		else:
 			return d[target-nums[i]], i
 
 
-print(twoSum([2,4,5,1,9],7))
+print(twoSum([2,4,5,11,9],6))
 
 #Runtime: 24 ms, faster than 99.83% of Python online submissions for Two Sum.
 #Memory Usage: 13.1 MB, less than 34.80% of Python online submissions for Two Sum.
