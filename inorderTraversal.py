c618c3193b06bc86501b107fd3099f314d84f897
nestorghh <nestorghh@outlook.com>
2020-08-12 03:26:48 -0400

    Create inorderTraversal.py

---
+++ inorderTraversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        
+        def helper(root,result):
+            
+            if root:
+                
+                if root.left:
+                    helper(root.left,result)
+                result.append(root.val)
+                if root.right:
+                    helper(root.right,result)
+
+        result=[] 
+        helper(root,result)
+        return result
+        
